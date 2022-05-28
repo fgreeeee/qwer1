c5483d1c512865857c269a9caa3869d560992413
Горбунов Владимир <42053880+fgreeeee@users.noreply.github.com>
2022-05-28 21:25:16 +0900

    Add files via upload

---
+++ src/page/GlavnayaPnl.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>GLAVNAYA</h1>
+  <div class="priv">
+  <h2>Добро пожаловать  в админ панель</h2>
+    <h3>В которой я наконец то разобрался!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GlavnayaPnl"
+}
+</script>
+
+<style scoped>
+.priv{
+  position: fixed;
+  top: 30%;
+  left: 32%;
+}
+</style>
